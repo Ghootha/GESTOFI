9fca97c4a89347c9e6a30e901c4431fe75dc53e1
Ghootha <mgonzmu@gmail.com>
2014-10-01 01:29:58 -0600

    DatePicker en consulta Doc

---
+++ js/datePicker.js
@@ -0,0 +1,7 @@
+ if (top.location != location) {
+    top.location.href = document.location.href ;
+  }
+        $(function(){
+            window.prettyPrint && prettyPrint();            
+            $('#dp2').datepicker();           
+        });
