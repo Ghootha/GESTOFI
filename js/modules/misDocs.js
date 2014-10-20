cbf198ce03e841a0706e8d79a7bb92087106029b
Ghootha <mgonzmu@gmail.com>
2014-10-19 22:21:42 -0600

    Editar en progreso, elimianr documento funciona pero sin confirmar

--- js/modules/misDocs.js
+++ js/modules/misDocs.js
@@ -13,26 +13,57 @@ app.controller("docController", function($scope, $http, $window) {
          $window.open('http://gestofi.com/'+ruta);
      };
 
-
-    /*
-     $scope.editDoc = function(id) {
-     if (id == 'new') {
+     $scope.incomplete = false;
+    
+     $scope.editDoc = function(id) {    
      $scope.edit = true;
-     $scope.incomplete = true;
-     $scope.fecha = '';
-     $scope.nombre = '';
-     $scope.codigo = '';
-     $scope.rol = '';
-     $scope.tipo = '';
-     } else {
-     $scope.edit = false;
      $scope.fecha = $scope.docs[id-1].fecha;
      $scope.nombre = $scope.docs[id-1].nombre;
      $scope.codigo = $scope.docs[id-1].codigo;
-     $scope.rol = $scope.docs[id-1].rol;
-     $scope.tipo = $scope.docs[id-1].tipo;
-     }
-     };*/
+     $scope.Role = $scope.docs[id-1].Role;
+     $scope.seguridad = $scope.docs[id-1].seguridad;     
+     $scope.tipo = $scope.docs[id-1].tipo;     
+     };
+
+     $scope.actualizaDoc = function(id) {
+        
+        var objetoJSON = {
+            "nombre": $scope.docs[id-1].nombre,            
+            "Role": $scope.docs[id-1].Role,
+            "tipo": $scope.docs[id-1].tipo,
+            "seguridad": $scope.docs[id-1].seguridad
+        };
+
+         $http.put("webservice/Documento/update/"+id, objetoJSON).success(
+                function(){
+                    $http.get("webservice/Documento")
+                        .success(function(response) {$scope.docs = null; $scope.docs = response;});
+                });
+     };
+
+     $scope.eliminaDoc = function(id) {
+         $http.delete("webservice/Documento/"+id).success( 
+            function(){
+                $http.get("webservice/Documento").success(function(response) {$scope.docs = response;});
+         });
+     };
+
+
+
+    $scope.$watch('fecha',function() {$scope.test();});
+    $scope.$watch('nombre',function() {$scope.test();});
+    $scope.$watch('codigo', function() {$scope.test();});    
+    $scope.$watch('Role', function() {$scope.test();});
+    $scope.$watch('seguridad', function() {$scope.test();});
+    $scope.$watch('tipo', function() {$scope.test();});    
+
+    $scope.test = function() {        
+        if ($scope.edit && (!$scope.fecha.length || !$scope.nombre.length ||
+            !$scope.codigo.length || !$scope.Role.length || !$scope.seguridad.length ||
+            !$scope.tipo.length)) {
+            $scope.incomplete = true;
+        }else{ $scope.incomplete = false; }
+    };
 
 
 
