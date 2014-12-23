08264f2259a9827b83cdfdf709d697c63f35e42b
Michael <mgonzmu@gmail.com>
2014-12-22 20:36:45 -0600

    .

--- js/datePicker.js
+++
@@ -1,7 +0,0 @@
- if (top.location != location) {
-    top.location.href = document.location.href ;
-  }
-        $(function(){
-            window.prettyPrint && prettyPrint();            
-            $('#dp2').datepicker();           
-        });
