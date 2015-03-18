63c8731920764c79355d363bb025c1ee5bcad3d2
JAAV30 <JoseA@iMac-de-Jose.local>
2015-03-17 23:32:18 -0600

    .
    .

--- ROUTES/js/modules/miPerfil.js
+++ ROUTES/js/modules/miPerfil.js
@@ -27,7 +27,7 @@ $http.get("webservice/User").success(function(response) {$scope.users = response
 $scope.modificarPerfil =function(){
 	
 	
-	
+	debugger;
 	var objeto={
 		"fullname": $scope.user.fullname,
 		"birthdate": $scope.user.birthdate,
@@ -45,6 +45,33 @@ $scope.modificarPerfil =function(){
 		alert("cambiado");
 
 	});
+
+};
+
+$scope.setContrasena=function(){
+	debugger;
+	var pass;
+	$http.get("webservice/User/"+$scope.user.id).success(function(response){
+		pass = response.passports[0].password;
+		alert(pass);
+	});
+	
+	/*if($scope.passNew===$scope.passNewConf && $scope.passNew!= ""){
+		if($scope.pass===$scope.passAct){
+			var o={
+				"password": $scope.passNew
+			};
+
+			$http.put("webservice/Passport/update/"+$scope.user.id,o).success(function(){
+		
+				alert("cambiado");
+
+			});
+
+		}
+
+	}*/
+	
 
 };    
 
