b0b7a523b5111d6170e87f2e5151e14f1a73f37f
JAAV30 <JoseA@iMac-de-Jose.local>
2015-05-06 13:47:57 -0600

    Solicitudes de Usuario
    Eliminar y seguridad

--- js/modules/misSolicitudesUser.js
+++ js/modules/misSolicitudesUser.js
@@ -8,25 +8,72 @@ app.controller("solicitudesUserController", function($scope, $http) {
     ]; 
     var objetoPendientes;var objetoHistorial;
     
-    $http.get("webservice/get_user").success(function(response){
-        objetoPendientes={
-        "usuario": response.user.username,
-        "lista":1
-        };
-        objetoHistorial={
-            "usuario": response.user.username,
-            "lista":2
-        };
+    $scope.$on('$viewContentLoaded', function () {
+      $http.get("webservice/get_user").success(function(response){
+                if(response.user == null){
+                  window.location.replace("index.html"); 
+                }else{     
+                  $scope.user= response.user;  
+                  var roleLogged = response.user.role;
+                  objetoPendientes={
+                    "usuario": response.user.username,
+                    "lista":1
+                  };
+                  objetoHistorial={
+                    "usuario": response.user.username,
+                    "lista":2
+                  };  
+                  $http.get("webservice/Role").success(function(response){
+                          $http.post("webservice/ReservaEquipo/findReservasUsuario",objetoPendientes).success(function(response){$scope.reservasPendientes=response;
+                            $http.post("webservice/ReservaEquipo/findReservasUsuario",objetoHistorial).success(function(response){$scope.reservasHistorial=response;});
+                          });
+                          var roles = response;
 
-        $http.post("webservice/ReservaEquipo/findReservasUsuario",objetoPendientes).success(function(response){$scope.reservasPendientes=response;
-            $http.post("webservice/ReservaEquipo/findReservasUsuario",objetoHistorial).success(function(response){$scope.reservasHistorial=response;});
+                          for(var i = 0; i<roles.length; i++) {           
+                              if(roles[i].nombre === roleLogged) {
+                                  
+                                  var seguridad=roles[i].seguridad;
+
+                                  if( seguridad == 'Ninguna' || seguridad == 'Baja' ){  
+                                     window.location.replace("paginaPrincipal.html"); 
+                                  }
+                                  
+                              }
+                          }
+                  });               
+              }
+      }).error(function(response, status, header, config){  
+              console.log("error en obtencion de usuario conectado");  
+      });
+    });
+
+    $scope.modalConfirmacion = function(objeto){
+        $scope.nombre=objeto.nombre;
+        $scope.horaInicio=objeto.horaInicio;
+        $scope.horaEntrega=objeto.horaEntrega;
+        $scope.fecha=objeto.fecha;
+        $('#Modal3').modal({backdrop:false}).one('click', '#confirm', function(){
+          $scope.borrarSolicitud(objeto.idReserva, objeto.ID);
+        });
+    };
     
+    $scope.borrarSolicitud=function(idReserva,pos){
+        
+        $http.get("webservice/ReservaEquipo/?idReserva="+idReserva).success(function(response){$scope.indice=response[0].id;
+            $http.get("webservice/ReservaEquipo/destroy/"+$scope.indice).success(function(response){
+                $http.get("webservice/Reserva/destroy/"+idReserva).success(function(response){
+                    for(var i=0; i<$scope.reservasPendientes.length;i++){
+                        if($scope.reservasPendientes[i].ID===pos){
+                            $scope.$apply($scope.reservasPendientes.splice(i, 1));
+                        }
+                    }
+                    //$http.post("webservice/ReservaEquipo/findReservasUsuario",objetoPendientes).success(function(response){$scope.reservasPendientes=response;});
+                });
+            });
         });
 
-    
         
-    });
-    
+    };
     
     
     
