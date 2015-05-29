c83859df5b23b57d3a4b5781122bc3e5dd7b0aa3
Natasha <natasha.artg@gmail.com>
2015-05-29 09:16:05 -0600

    comentado
    ...

--- js/modules/miAgenda.js
+++ js/modules/miAgenda.js
@@ -5,24 +5,24 @@ app.controller("agendaController", function($scope, $http, $window, $location, $
 
 		var req = {
 			 method: 'GET',
-			 url: 'webservice/Agenda/findActByUser',
+			 url: 'webservice/Agenda/findActByUser', //Filtra actividades por usuario
 			 dataType: 'json',
 			 data: '',
 			 headers: {
 			   'Content-Type': "application/json; charset=utf-8"
 			 }
-			 //url: 'webservice/Agenda', para ver todas las act
+			 
 		};
 		
-		$scope.$on('$viewContentLoaded', function () {
+		$scope.$on('$viewContentLoaded', function () { //Todo aquello que se quiera cargar al inicio de cada pagina
 		  	$http.get("webservice/get_user").success(function(response){
-		            if(response.user == null){
+		            if(response.user == null){ //valida que exista un usuario conectado, si no es asi lo devuelve a la pagian de login
 		               window.location.replace("index.html"); 
 		            }else{ 
 		                $scope.user= response.user;               
 		                var roleLogged = response.user.role;
 		            
-		                $http.get("webservice/Role").success(function(response){
+		                $http.get("webservice/Role").success(function(response){ // obtiene el role del usuario conectado, luego busca cual es la seguridad de dicho role
 		                        var roles = response;
 
 		                        for(var i = 0; i<roles.length; i++) {           
@@ -30,7 +30,7 @@ app.controller("agendaController", function($scope, $http, $window, $location, $
 		                                
 		                                var seguridad=roles[i].seguridad;
 
-		                                if( seguridad == 'Ninguna'){  
+		                                if( seguridad == 'Ninguna'){  //Seguridad de las vistas que usen este controlador
 		                                     window.location.replace("paginaPrincipal.html"); 
 		                                }
 		                                
@@ -43,23 +43,27 @@ app.controller("agendaController", function($scope, $http, $window, $location, $
 		    });
 		});
 
-		$scope.loadTags = function(query) {
+		$scope.loadTags = function(query) { //Trae los usuarios al tag 
         return $http.get("webservice/User");
 		};
 	
+//------------------------Agregar una actividad a la Agenda----------------------------------------------------//
 
 		$scope.agregarActividad = function(){ 
 			debugger;
+			//variables utilizadas para convertir la fecha al formato deseado
 			var k=$scope.horaSA.split(":");
 			var w=$scope.horaEA.split(":");
 			var i=$scope.startA.split("-");
 			var inicio=new Date(i[2],i[1]-1,i[0],k[0]-6,k[1],"00");
 			var f=$scope.endA.split("-");
 			var ffinal=new Date(f[2],f[1]-1,f[0],w[0]-6,w[1],"00");
+			// variables usadas para manejar los invitados
 			var objetoJSON;
 			var invitados=$scope.invitadoA;
 			var auxinvitado="";
 			
+			//Invitado por invitado se envia una notificacion 
 			if(invitados.length != 0 ){
 				var auxinvitado=",";
 				invitados.forEach(function(entry){		
@@ -76,13 +80,12 @@ app.controller("agendaController", function($scope, $http, $window, $location, $
 					};
 				
 					$http.put("webservice/notificaciones/create", objetoJSON).success(function(response){});
-					//$route.reload();
-					
+									
 				});	
 			}
-			
+			//validacion de fechas de la actividad
 			if(ffinal > inicio){
-			
+			//Se crea el objeto completo para crear la actividad en la base
 			objetoJSON = {
 				"title": $scope.actividadA,
 				"autor":  $scope.user.username,
@@ -92,7 +95,7 @@ app.controller("agendaController", function($scope, $http, $window, $location, $
 				"start": inicio,
 				"end": ffinal
 			};
-			
+			// alertas,cierre y refresque del modal
 			$http.post("webservice/Agenda/create", objetoJSON).success(function(response){				
 				$route.reload();
 				$('#calendar').fullCalendar('renderEvent', objetoJSON, true);
@@ -112,34 +115,32 @@ app.controller("agendaController", function($scope, $http, $window, $location, $
 			}
 					
          };
-		 
+//------------------------Agregar una actividad a la Agenda----------------------------------------------------//
+
+
+//------------------------Actualizar una actividad a la Agenda----------------------------------------------------//		 
 		
 	$scope.actualizaAct = function() {  
 		debugger;
+		//variables auxiliares para el manejo de fechas 
 				var f;
 				var i;
 				var objetoJSON;
-				// for(var x = 0; x<$scope.actividades.length; x++) {
-				// 	if($scope.actividades[x].id === $scope.id) {
-					
-								var k=$scope.horaS.split(":");
-								var w=$scope.horaE.split(":");
-								var i=$scope.start.split("-");
-								var inicio=new Date(i[2],i[1]-1,i[0],k[0]-6,k[1],"00");
-								var f=$scope.end.split("-");
-								var ffinal=new Date(f[2],f[1]-1,f[0],w[0]-6,w[1],"00");
-							// var invitados=$scope.invitado;
-							// var auxinvitado=",";
-							// invitados.forEach(function(entry){		
-							// 	auxinvitado=auxinvitado+entry.fullname+",";		
-							// });											
-									
+						
+				var k=$scope.horaS.split(":");
+				var w=$scope.horaE.split(":");
+				var i=$scope.start.split("-");
+				var inicio=new Date(i[2],i[1]-1,i[0],k[0]-6,k[1],"00");
+				var f=$scope.end.split("-");
+				var ffinal=new Date(f[2],f[1]-1,f[0],w[0]-6,w[1],"00");				
+																	
+						//validacion de fechas de la actividad			
 						if(ffinal > inicio){
+						//objeto actualizado
 							objetoJSON = {
 								"title": $scope.title,
 								"autor":  $scope.user.username,
 								"lugar": $scope.lugar,
-								//"invitado": auxinvitado,
 								"descripcion": $scope.descripcion,
 								"start": inicio,
 								"end": ffinal
@@ -166,14 +167,14 @@ app.controller("agendaController", function($scope, $http, $window, $location, $
 				$scope.end="";
 				bootbox.alert("No se edito la actividad porque la fecha final es anterior a la fecha de inicio");
 			}
-			//}
-         // } 
-  };
-		 
-		 
-		 $scope.eliminarAct = function() {
+	};
+
+//------------------------Actualizar una actividad a la Agenda----------------------------------------------------//	
+
+//------------------------Eliminar una actividad a la Agenda----------------------------------------------------//	 
+		 $scope.eliminarAct = function() { 
 			 $('#Modal4').modal({ backdrop: false})
-			.one('click', '#confirm', function () {
+			.one('click', '#confirm', function () { //al confirmar eliminar, borra la actividad de la base  
 				
 			  $http.get("webservice/Agenda/destroy/"+$scope.id).success(
 					function(){
@@ -195,8 +196,9 @@ app.controller("agendaController", function($scope, $http, $window, $location, $
 			});
 			
 		 };
+//------------------------Eliminar una actividad a la Agenda----------------------------------------------------//	
 			
-	 
+//------------------------Muestra la Agenda----------------------------------------------------//	 
 		$http(req).success(function(data, response){
 			$scope.actividades=data;
 			
@@ -209,10 +211,10 @@ app.controller("agendaController", function($scope, $http, $window, $location, $
 					
 				},
 				editable: true,
-				eventLimit: true, // allow "more" link when too many events
+				eventLimit: true, 
 				
 				events:data,
-				
+				//al dar click a una actividad
 				eventClick: function(calEvent, jsEvent, view) {
 													
 										
