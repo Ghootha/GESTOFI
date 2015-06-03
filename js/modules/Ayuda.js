85d2b560e0e80dde10887b12533de5082171cd90
Michael <mgonzmu@gmail.com>
2015-06-03 01:39:17 -0600

    Adios ayuda de login

--- js/modules/Ayuda.js
+++ js/modules/Ayuda.js
@@ -9,7 +9,6 @@ app.controller("AyudaController", function($scope, $http, $timeout, $location, $
         {nombre: "Configuración de Usuarios", video:"configUsuarios.mp4"},
         {nombre: "Control de Reservas", video:"controlReservas.mp4"},
         {nombre: "Correspondencia", video:"correspondencia.mp4"},
-        {nombre: "Login", video:"login.mp4"},
         {nombre: "Repositorio", video:"repositorio.mp4"},
         {nombre: "Responder Solicitudes", video:"responderSolicitudes.mp4"},
         {nombre: "Solicitudes", video:"Solicitudes.mp4"}
